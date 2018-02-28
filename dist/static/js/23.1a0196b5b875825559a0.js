02d71c8c4657f5ca00c83845932cda37429a51c3
Nick <xuyong@surong360.com>
2018-02-28 17:34:08 +0800

    删除文件

--- dist/static/js/23.1a0196b5b875825559a0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([23],{1014:function(n,e,t){var r=t(105)(null,t(1116),null,null);n.exports=r.exports},1116:function(n,e){n.exports={render:function(){var n=this,e=n.$createElement;return(n._self._c||e)("section",[n._v("page6...\n")])},staticRenderFns:[]}}});
-//# sourceMappingURL=23.1a0196b5b875825559a0.js.map
